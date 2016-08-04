e45e2d54d84c0eda1b262aec75fcb0abd9325513
Tielman <tielman.mailbox@gmail.com>
2016-08-04 22:04:21 +0200

    Found method to play audio on mobile

--- README.md
+++ README.md
@@ -3,8 +3,11 @@ A short example to demonstrate how audio can be recorded with the new MediaRecor
 
 The MediaRecorder is now available natively in some browsers. This example definitely works from Chrome v49 on desktop. On Android, add the Crosswalk browser to the project. The latest stable version of Crosswalk at the time of writing is Chrome v49, which has the MediaRecorder API baked in.
 
-What has been done:
- - Created a new Ionic project: $ionic start AudioRecorder blank
- - cd into folder: $cd AudioRecorder
- - Added the Crosswalk browser: $ionic browser add crosswalk
- - Added the record directive to app.js
+To run the project:
+ - Clone the repo
+ - install the dependencies: bower install
+ - install the plugins:
+   - Crosswalk browser: $ cordova plugins add cordova-plugin-crosswalk-webview
+   - File API: $ cordova plugins add cordova-plugin-file
+   - Media (for permission, could probably just necessary permissions): $ cordova plugins add cordova-plugin-media
+ - Run the project on either a browser or on Android
--- www/js/app.js
+++ www/js/app.js
@@ -1,28 +1,28 @@
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
 
 .directive('recordDirective', RecordDirective);
 
-RecordDirective.$inject = [];
+RecordDirective.$inject = ['$ionicPlatform', '$cordovaFile', '$cordovaMedia', '$timeout'];
 
-function RecordDirective() {
+function RecordDirective($ionicPlatform, $cordovaFile, $cordovaMedia, $timeout) {
     var ddo = {
         scope: {},
         restrict: 'E',
         template: [
-            '<button class="button" ng-click="record()">Record</button>',
-            '<button class="button" ng-click="stop()">Stop</button>',
+            '<button class="button icon ion-record" ng-class="{\'button-assertive\': recording}" ng-click="action()"></button>',
+            '<span ng-if="initialiseAudio"><ion-spinner></ion-spinner></span>',
             '<div><audio controls></audio></div>'
         ].join(''),
         link: function(scope, element, attributes) {
@@ -30,48 +30,86 @@ function RecordDirective() {
             var mediaStream;
             var recordedBlobs = [];
             var audioElement = element.find('audio')[0];
-            scope.record = function() {
-                recordedBlobs = [];
-                console.log('Record clicked');
-                if (navigator.webkitGetUserMedia) {
-                   navigator.webkitGetUserMedia (
-                      {
-                         audio: true,
-                         video: false
-                      },
-                      function(stream) {
-                          console.log('got Stream');
-                          mediaStream = stream;
-                          mediaRecorder = new MediaRecorder(stream, {
-                              mimeType: 'audio/webm'
-                          });
-                          mediaRecorder.ondataavailable = function (event) {
-                              console.log('data available');
-                              recordedBlobs.push(event.data);
-                          };
-                          mediaRecorder.start(10);
-                      },
-                      function(err) {
-                         console.log('The following gUM error occured: ' + err);
-                      }
-                   );
+            scope.recording = false;
+            scope.initialiseAudio = false;
+            scope.action = function() {
+                if (!scope.recording) {
+                    scope.recording = true;
+                    recordedBlobs = [];
+                    if (navigator.webkitGetUserMedia) {
+                        scope.initialiseAudio = true;
+                        navigator.webkitGetUserMedia (
+                            {
+                                audio: true,
+                                video: false
+                            },
+                            function(stream) {
+                                $timeout(function() {
+                                    scope.initialiseAudio = false; //since it happens outside of angular world
+                                });
+                                mediaStream = stream;
+                                mediaRecorder = new MediaRecorder(stream, {
+                                    mimeType: 'audio/webm'
+                                });
+                                mediaRecorder.ondataavailable = function (event) {
+                                    if (scope.recording) {
+                                        recordedBlobs.push(event.data);
+                                    }
+                                };
+                                mediaRecorder.start(10);
+                            },
+                            function(err) {
+                                $timeout(function() {
+                                    scope.initialiseAudio = false; //since it happens outside of angular world
+                                });
+                                console.log('The following gUM error occured: ' + err);
+                            }
+                        );
+                    } else {
+                        console.log('getUserMedia not supported on your browser!');
+                    }
                 } else {
-                   console.log('getUserMedia not supported on your browser!');
+                    scope.recording = false;
+                    mediaRecorder.stop();
+                    mediaStream.getTracks().forEach(function(track) {
+                        console.log('stopping stream track');
+                        track.stop();
+                    });
+                    var superAudioBuffer = new Blob(recordedBlobs, {
+                        type: 'audio/webm'
+                    });
+                    console.log(superAudioBuffer);
+                    var audioUrl = URL.createObjectURL(superAudioBuffer);
+                    if (document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1) {
+                        //Cordova
+                        console.log(audioUrl);
+                        $ionicPlatform.ready(function() {
+                            $cordovaFile.writeFile(cordova.file.cacheDirectory, 'temp.webm', superAudioBuffer, true)
+                            .then(function(success) {
+                                var mediaSource = cordova.file.cacheDirectory + 'temp.webm';
+                                audioElement.src = mediaSource;
+                                audioElement.play();
+                            }, function(error) {
+                                console.log('Write error:');
+                                console.log(error);
+                            });
+                        });
+                    } else {
+                        //On browser
+                        audioElement.src = audioUrl;
+                        audioElement.play();
+                    }
                 }
             };
-            scope.stop = function() {
-                console.log('Stop button clicked');
-                mediaRecorder.stop();
-                mediaStream.getTracks().forEach(function(track) {
-                    console.log('stopping stream track');
-                    track.stop();
+            scope.$on('$destroy', function() {
+                $cordovaFile.removeFile(cordova.file.cacheDirectory, "temp.webm")
+                .then(function (success) {
+                    console.log('File removal successful');
+                }, function (error) {
+                    console.log('File removal error: ');
+                    console.log(error);
                 });
-                var superAudioBuffer = new Blob(recordedBlobs, {
-                    type: 'audio/webm'
-                });
-               var audioUrl = URL.createObjectURL(superAudioBuffer);
-               audioElement.src = audioUrl;
-            };
+            });
         }
     };
     return ddo;
