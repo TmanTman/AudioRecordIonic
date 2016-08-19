8512010fb5adf057e7a751beafe0097895c2e927
Tielman <tielman.mailbox@gmail.com>
2016-08-19 06:28:33 +0200

    Wrote own filewrite implementation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # AudioRecordIonic
 A short example to demonstrate how audio can be recorded with the new MediaRecorder API
 
-The MediaRecorder is now available natively in some browsers. This example definitely works from Chrome v49 on desktop. On Android, add the Crosswalk browser to the project. The latest stable version of Crosswalk at the time of writing is Chrome v49, which has the MediaRecorder API baked in.
+The MediaRecorder has been available for a while now on the most popular browser, [see here](https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder). This example definitely works from Chrome v49 on desktop. To make sure that this is available in your Ionic project, make use of the Crosswalk browser - Chrome 49 features have been in crosswalk since version crosswalk-19.49.514.4. The latest stable version of Crosswalk will have the MediaRecorder included.
 
 To run the project:
  - Clone the repo
--- www/js/audioPlayerCordova.service.js
+++ www/js/audioPlayerCordova.service.js
@@ -11,25 +11,79 @@
             $ionicPlatform.ready(function() {
                 var storageLocation = cordova.file.cacheDirectory;
                 var filename = 'temp.webm';
-                $window.resolveLocalFileSystemURL(storageLocation + filename, function(fileEntry) {
-                    fileEntry.createWriter(function(fileWriter) {
-                        fileWriter.onwriteend = function() {
-                            console.log('Successfully wrote file');
-                            console.log(fileEntry);
-                            deferred.resolve(storageLocation + filename);
-                        };
-                        fileWriter.onerror = function(e) {
-                            console.log('Failed to write: ');
-                             console.log(e);
-                        };
-                        fileWriter.write(pAudioBlob);
-                    });
-                }, function(e) {
-                    console.log('Failed to resolve file: ');
-                    console.log(e);
+                writeFile(storageLocation, filename, pAudioBlob, true)
+                .then(function(success) {
+                    deferred.resolve(storageLocation + filename);
+                }).catch(function(error){
+                    console.log('Error:');
+                    console.log(error);
                 });
             });
             return deferred.promise;
         };
+
+        var errorCodes = {
+            1: 'NOT_FOUND_ERR',
+            2: 'SECURITY_ERR',
+            3: 'ABORT_ERR',
+            4: 'NOT_READABLE_ERR',
+            5: 'ENCODING_ERR',
+            6: 'NO_MODIFICATION_ALLOWED_ERR',
+            7: 'INVALID_STATE_ERR',
+            8: 'SYNTAX_ERR',
+            9: 'INVALID_MODIFICATION_ERR',
+            10: 'QUOTA_EXCEEDED_ERR',
+            11: 'TYPE_MISMATCH_ERR',
+            12: 'PATH_EXISTS_ERR'
+        };
+
+        function writeFile(path, fileName, text, replaceBool) {
+          var q = $q.defer();
+          if ((/^\//.test(fileName))) {
+            q.reject('file-name cannot start with \/');
+          }
+          replaceBool = replaceBool ? false : true;
+          var options = {
+            create: true,
+            exclusive: replaceBool
+          };
+          try {
+            $window.resolveLocalFileSystemURL(path, function (fileSystem) {
+              fileSystem.getFile(fileName, options, function (fileEntry) {
+                fileEntry.createWriter(function (writer) {
+                  if (options.append === true) {
+                    writer.seek(writer.length);
+                  }
+
+                  if (options.truncate) {
+                    writer.truncate(options.truncate);
+                  }
+
+                  writer.onwriteend = function (evt) {
+                    if (this.error) {
+                      q.reject(this.error);
+                    } else {
+                      q.resolve(evt);
+                    }
+                  };
+                  writer.write(text);
+                  q.promise.abort = function () {
+                    writer.abort();
+                  };
+                });
+              }, function (error) {
+                error.message = errorCodes[error.code];
+                q.reject(error);
+              });
+            }, function (err) {
+              err.message = errorCodes[err.code];
+              q.reject(err);
+            });
+          } catch (e) {
+            e.message = errorCodes[e.code];
+            q.reject(e);
+          }
+          return q.promise;
+      };
     }
 })();
